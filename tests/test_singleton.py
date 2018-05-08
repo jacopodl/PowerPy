ca5608d19fc2bdb2a65c2ef80275c4cc4165d329
jacopodl <jacopo.delu@gmail.com>
2018-05-08 19:36:28 +0200

    Added Singleton decorator

--- tests/test_singleton.py
+++ tests/test_singleton.py
@@ -1,14 +1,25 @@
 import unittest
 
-from powerpy.singleton import Singleton
+from powerpy.singleton import Singleton, SingletonMeta
 
 
-class Single1(metaclass=Singleton):
+@Singleton
+class Single1:
     def __init__(self, raw: str = None):
         self.raw = raw
 
 
-class Single2(metaclass=Singleton):
+@Singleton
+class Single2:
+    pass
+
+
+class SingleMeta1(metaclass=SingletonMeta):
+    def __init__(self, raw: str = None):
+        self.raw = raw
+
+
+class SingleMeta2(metaclass=SingletonMeta):
     pass
 
 
@@ -22,4 +33,17 @@ class TestSingleton(unittest.TestCase):
     def test_singleton_nq(self):
         s1 = Single1()
         s2 = Single2()
+        self.assertNotEqual(s1, s2)
+
+
+class TestSingletonMeta(unittest.TestCase):
+    def test_singleton_eq(self):
+        s1 = SingleMeta1("t1")
+        s2 = SingleMeta1("t2")
+        self.assertEqual(s1, s2)
+        self.assertEqual(s1.raw, s2.raw)
+
+    def test_singleton_nq(self):
+        s1 = SingleMeta1()
+        s2 = SingleMeta2()
         self.assertNotEqual(s1, s2)
